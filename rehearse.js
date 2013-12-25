15c4be75f4dbf852d1ebfaa06dd9945a0cd06287
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-24 21:04:19 -0300

    format

--- rehearse.js
+++ rehearse.js
@@ -3,12 +3,14 @@ var JSONB = require('json-buffer');
 
 module.exports = function () {
     var start = new Date();
-    var stamp = through(function (data) {
+
+    function stamp (data) {
         var now = new Date();
         var diff = now - start;
         var step = JSONB.stringify({ time: diff, data: data });
         this.queue(step + '\n');
         start = now;
-    });
-    process.stdin.pipe(stamp).pipe(process.stdout);
+    };
+
+    process.stdin.pipe(through(stamp)).pipe(process.stdout);
 };
